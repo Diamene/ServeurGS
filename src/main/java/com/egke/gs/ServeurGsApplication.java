d1cec60afd611c2c0fe1abc5b64afe65eb5a7bfd
KEITA <gimba.keita.external@atos.net>
2020-07-04 21:45:05 +0200

    init projet

---
+++ src/main/java/com/egke/gs/ServeurGsApplication.java
@@ -0,0 +1,13 @@
+package com.egke.gs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egke/gs/ServeurGsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egke.gs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
